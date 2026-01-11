3d79d896a6a4350e52d6ef2ba90668354cebe699
dani162 <34603476+dani162@users.noreply.github.com>
2026-01-11 11:52:02 +0100

    more demo

---
+++ demo-fedora-forks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUM_FORKS=$1
+INIT_SERVER_ADDRESS=192.168.51.10:3333
+START_FORK_COMMAND="./target/release/fork init-server 0.0.0.0:0 --init-server $INIT_SERVER_ADDRESS --save-config-dir ./config/ || sleep 100"
+
+ptyxis --new-window -- bash -c "
+NUM_FORKS=$NUM_FORKS
+for ((i = 0; i < NUM_FORKS; i++)); do
+  ptyxis --tab --title \"Fork $i\" -- bash -c \"$START_FORK_COMMAND\"
+done
+"
