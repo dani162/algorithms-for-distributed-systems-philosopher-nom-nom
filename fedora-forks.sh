be5198de6229122306444d9dc59908e732901f85
dani162 <34603476+dani162@users.noreply.github.com>
2026-01-11 11:45:42 +0100

    not needed

--- fedora-forks.sh
+++ fedora-forks.sh
@@ -4,7 +4,6 @@ NUM_FORKS=$1
 INIT_SERVER_ADDRESS=127.0.0.1:3333
 START_FORK_COMMAND="./target/release/fork init-server 0.0.0.0:0 --init-server $INIT_SERVER_ADDRESS --save-config-dir ./config/ || sleep 100"
 
-cargo build --release
 ptyxis --new-window -- bash -c "
 NUM_FORKS=$NUM_FORKS
 for ((i = 0; i < NUM_FORKS; i++)); do
